c87bf20390f52cc5e4ff07d524ddebe4bb572e9f
Radical Egg <luther.jaymen@gmail.com>
2022-09-18 16:08:55 -0700

    updated sha

--- Formula/plumbus.rb
+++ Formula/plumbus.rb
@@ -2,7 +2,7 @@ class Plumbus < Formula
   desc "Copy command output to your clipboard"
   homepage "https://github.com/Radical-Egg/plumbus"
   url "https://github.com/Radical-Egg/plumbus/releases/download/plumbus-1.0.4-1/plumbus-mac.tar.gz"
-  sha256 "3f610da991de929d74f41b327de3d72bdd58922218c2dcc081ea97c49a00cae8"
+  sha256 "3f85324098260e23c449047c332de80e817ca4cf2030e34229aff73d896420a7"
   version "1.0.4"
 
   def install
